9aaf0e2e9d6056e424ae7f8315ce24227e314f54
Ivan Aleksandrov <58612800+alpha-arietis@users.noreply.github.com>
2020-06-21 22:02:44 +1200

    updated "homepage" to point to new URL

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coronavirusupdate.nz/precache-manifest.e64c7abf5b1e4e055c900ffebbf3764c.js"
+  "/precache-manifest.d1a3bb72d6d92915181870f34d59c46b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/coronavirusupdate.nz/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
